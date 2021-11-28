fd0bc5c966812b2ec1113fc86de8d80fb1aee27f
michel <leandrasilvap074@gmail.com>
2021-11-28 00:40:05 -0300

    aprimored the mode that collect the data

--- notice/main/views.py
+++ notice/main/views.py
@@ -4,48 +4,64 @@ import requests
 
 from .models import Notice
 
-# Create your views here.
-def get_all_news():
-    categorys = ['technology']
+class get_news():
+    links = {
+        1: 'https://www.bbc.com/portuguese/topics/c404v027pd4t', #technology
+        2: 'https://www.bbc.com/portuguese/topics/cr50y580rjxt', #science
+        3: 'https://www.bbc.com/portuguese/topics/cvjp2jr0k9rt', #economy
+        4: 'https://www.bbc.com/portuguese/topics/c340q430z4vt', #health
+    }
+    categorys = {1: 'technology', 2: 'science', 3: 'economy', 4: 'health'}
 
-    page = requests.get(url='https://www.bbc.com/portuguese/topics/c404v027pd4t').text
-    soup = BeautifulSoup(page, 'lxml')
+    def get_all(self, category: int):
+        page = requests.get(url=self.links[category]).text
+        soup = BeautifulSoup(page, 'lxml')
 
-    title = soup.find_all('span', class_='lx-stream-post__header-text')
-    titulo = []
-    for i in title:
-        titulo.append(i.text)
-    
-    description = soup.find_all('p', class_='lx-stream-related-story--summary')
-    descricao = []
-    for i in description:
-        descricao.append(i.text)
+        title = soup.find_all('span', class_='lx-stream-post__header-text')
+        titulo = []
+        for i in title:
+            titulo.append(i.text)
 
-    date = soup.find_all('span', class_='qa-post-auto-meta')
-    data = []
-    for i in date:
-        data.append(i.text)
+        description = soup.find_all('p', class_='lx-stream-related-story--summary')
+        descricao = []
+        for i in description:
+            descricao.append(i.text)
 
-    image = soup.find_all('img', class_='lx-stream-related-story--index-image')
-    image_link = []
-    for i in image:
-        image_link.append(i['src'])
+        date = soup.find_all('span', class_='qa-post-auto-meta')
+        data = []
+        for i in date:
+            data.append(i.text)
 
-    for i in range(len(titulo)):
-        exist = Notice.objects.filter(title=titulo[i]).exists
-        if not exist:
-            new = Notice(
-                category=categorys[0],
-                title=titulo[i],
-                description=descricao[i],
-                date_post=data[i],
-                image=image_link[i],
-            )
-            new.save()
+        image = soup.find_all('img', class_='lx-stream-related-story--index-image')
+        image_link = []
+        for i in image:
+            image_link.append(i['src'])
 
+        for i in range(len(titulo)):
+            exist = Notice.objects.filter(title=titulo[i]).exists
+            if not exist:
+                new = Notice(
+                    category=self.categorys[category],
+                    title=titulo[i],
+                    description=descricao[i],
+                    date_post=data[i],
+                    image=image_link[i],
+                )
+                new.save()
 
 def index(request):
-    get_all_news()
-    news = Notice.objects.all().order_by('-date')
-    # Reserved.objects.filter(client=client_id).order_by('-check_in')
-    return render(request, 'main/index.html', {'news': news})
+    news = get_news()
+    for i in range(4):
+        news.get_all(i+1)
+
+    technology = Notice.objects.filter(category='technology').order_by('-date')
+    science = Notice.objects.filter(category='science').order_by('-date')
+    economy = Notice.objects.filter(category='economy').order_by('-date')
+    health = Notice.objects.filter(category='health').order_by('-date')
+
+    return render(request, 'main/index.html', {
+        'technology': technology,
+        'science': science,
+        'economy': economy,
+        'health': health,
+    })
