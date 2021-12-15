74fa86cf81728db222ed61629cb55d1f4074ae01
michel <michel@pop-os.localdomain>
2021-12-15 20:45:16 -0300

    new improvements

--- core/Main.py
+++ core/Main.py
@@ -5,28 +5,16 @@ html = requests.get(url="https://redecanais.wf/").text
 
 soup = Soup(html, "lxml")
 
-section_main = soup.find("div", class_="col-md-12")
-section_name = section_main.h2.text
+section_main = soup.find_all("div", class_="col-md-12")
 
-movies_main = section_main.find_all("h3")
+for movie in section_main:
+    title_link = movie.find('a', class_="ellipsis")
+    img = movie.find('img', class_="img-responsive")
 
-filmes = []
-links = []
-for movie_name in movies_main:
-    filmes.append(movie_name.a.text)
-    links.append(movie_name.a['href'])
-
-images = []
-movies_main = section_main.find_all("img", class_="img-responsive")
-for movie_img in movies_main:
-    images.append(movie_img['data-echo'])
-
-for i in range(len(filmes)):
     print(
         f'''
-            {filmes[i]}
-            {links[i]}
-            {images[i]}
-            \n
+            Nome: {title_link.text}
+            Link: {title_link['href']}
+            Img: {img['data-echo']}
         '''
     )
