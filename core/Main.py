0e92cc525e59401a42c7c841f6e7ea7c5a8a1fb6
michel <michel@pop-os.localdomain>
2021-12-17 04:22:03 -0300

    new improvement

--- core/Main.py
+++ core/Main.py
@@ -11,10 +11,12 @@ for movie in section_main:
     title_link = movie.find('a', class_="ellipsis")
     img = movie.find('img', class_="img-responsive")
 
-    print(
-        f'''
-            Nome: {title_link.text}
-            Link: {title_link['href']}
-            Img: {img['data-echo']}
-        '''
-    )
+    print("\nTitle: ", title_link.text,"\nLink: ", title_link['href'],"\nImg: ", img['data-echo'])
+
+    # print(
+    #     f'''
+    #         Nome: {title_link.text}
+    #         Link: {title_link['href']}
+    #         Img: {img['data-echo']}
+    #     '''
+    # )
